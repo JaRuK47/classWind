4b48b17ad2aca72b3a5129b91b391102d57d96e1
KuRaJ <Kuraj.com>
2026-03-16 14:45:00 +0800

    create operator

--- klaswind/NumberSystem.cs
+++ klaswind/NumberSystem.cs
@@ -21,7 +21,89 @@ namespace klaswind
 
         public string Verbose()
         {
-            return String.Format("{0} {1}", this.value, this.type);
+            string typeVerbose = "";
+            switch (this.type)
+            {
+                case MeasureType.n2:
+                    typeVerbose = "с.с. 2";
+                    break;
+                case MeasureType.n8:
+                    typeVerbose = "с.с. 8";
+                    break;
+                case MeasureType.n10:
+                    typeVerbose = "с.с. 10";
+                    break;
+                case MeasureType.n16:
+                    typeVerbose = "с.с. 16";
+                    break;
+            }
+            return String.Format("{0} {1}", this.value, typeVerbose);
+        }
+
+        public static NumberSystem operator +(NumberSystem instance, double number)
+        {
+            return new NumberSystem(instance.value + number, instance.type); ;
+        }
+
+        public static NumberSystem operator +(double number, NumberSystem instance)
+        {
+            return instance + number;
+        }
+
+        public static NumberSystem operator *(NumberSystem instance, double number)
+        {
+            return new NumberSystem(instance.value * number, instance.type); ;
+        }
+
+        public static NumberSystem operator *(double number, NumberSystem instance)
+        {
+            return instance * number;
+        }
+
+        public static NumberSystem operator -(NumberSystem instance, double number)
+        {
+            return new NumberSystem(instance.value - number, instance.type); ;
+        }
+
+        public static NumberSystem operator -(double number, NumberSystem instance)
+        {
+            return instance - number;
+        }
+
+        public static NumberSystem operator /(NumberSystem instance, double number)
+        {
+            return new NumberSystem(instance.value / number, instance.type); ;
+        }
+
+        public static NumberSystem operator /(double number, NumberSystem instance)
+        {
+            return instance / number;
+        }
+
+        public NumberSystem To(MeasureType newType)
+        {
+            var newValue = this.value;
+            if (this.type == MeasureType.n10)
+            {
+                switch (newType)
+                {
+                    case MeasureType.n10:
+                        newValue = this.value;
+                        break;
+                    case MeasureType.n2:
+                        newValue = this.value / 1000;
+                        break;
+                    // если в  а.е.
+                    case MeasureType.n8:
+                        newValue = this.value / 149597870700;
+                        break;
+                    // если в парсек
+                    case MeasureType.n16:
+                        newValue = this.value / (3.0856776 * Math.Pow(10, 16));
+                        break;
+                }
+            }
+            return new NumberSystem(newValue, newType);
         }
     }
 }
--- klaswindTests/NumberSystemTests.cs
+++ klaswindTests/NumberSystemTests.cs
@@ -14,8 +14,49 @@ namespace klaswind.Tests
         [TestMethod()]
         public void VerboseTest()
         {
-            var length = new NumberSystem(10, MeasureType.n10);
-            Assert.AreEqual("10 n10", length.Verbose());
+            var val = new NumberSystem(10, MeasureType.n10);
+            Assert.AreEqual("10 с.с. 10", val.Verbose());
+
+            val = new NumberSystem(10, MeasureType.n8);
+            Assert.AreEqual("10 с.с. 8", val.Verbose());
+
+            val = new NumberSystem(10, MeasureType.n2);
+            Assert.AreEqual("10 с.с. 2", val.Verbose());
+
+            val = new NumberSystem(10, MeasureType.n16);
+            Assert.AreEqual("10 с.с. 16", val.Verbose());
+        }
+
+        [TestMethod()]
+        public void AddNumberTest()
+        {
+            var val = new NumberSystem(1, MeasureType.n10);
+            val = val + 4.25;
+            Assert.AreEqual("5,25 с.с. 10", val.Verbose());
+        }
+
+        [TestMethod()]
+        public void SubNumberTest()
+        {
+            var val = new NumberSystem(3, MeasureType.n10);
+            val = val - 1.75;
+            Assert.AreEqual("1,25 с.с. 10", val.Verbose());
+        }
+
+        [TestMethod()]
+        public void MulByNumberTest()
+        {
+            var val = new NumberSystem(3, MeasureType.n10);
+            val = val * 3;
+            Assert.AreEqual("9 с.с. 10", val.Verbose());
+        }
+
+        [TestMethod()]
+        public void DivByNumberTest()
+        {
+            var val = new NumberSystem(3, MeasureType.n10);
+            val = val / 3;
+            Assert.AreEqual("1 с.с. 10", val.Verbose());
         }
     }
 }
