c7c83d3995a2a8c92f21da6c0bc89c9865df0e05
KuRaJ <Kuraj.com>
2026-03-16 13:42:16 +0800

    create repos

---
+++ klaswind/NumberSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klaswind
+{
+    public enum MeasureType { n2, n8, n10, n16 };
+    public class NumberSystem
+    {
+        private double value;
+        private MeasureType type;
+
+        public NumberSystem(double value, MeasureType type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+
+        public string Verbose()
+        {
+            return String.Format("{0} {1}", this.value, this.type);
+        }
+    }
+}
---
+++ klaswindTests/NumberSystemTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using klaswind;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klaswind.Tests
+{
+    [TestClass()]
+    public class NumberSystemTests
+    {
+        [TestMethod()]
+        public void VerboseTest()
+        {
+            var length = new NumberSystem(10, MeasureType.n10);
+            Assert.AreEqual("10 n10", length.Verbose());
+        }
+    }
+}
+
+namespace klaswindTests
+{
+    internal class NumberSystemTests
+    {
+    }
+}
